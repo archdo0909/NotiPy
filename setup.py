48baee46549d7fa11f4651cc873601af73a789a4
eddie Kim <archdo0909@gmail.com>
2020-04-27 00:44:41 +0900

    Create setup.py and requirement.txt

---
+++ setup.py
@@ -0,0 +1,35 @@
+
+from setuptools import setup, find_packages
+
+from os import path
+
+def get_readme():
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+        readme = readme_file.read()
+    return readme
+
+def get_requirements():
+    here = path.abspath(path.dirname(__file__))
+    with open(path.join(here, 'requirements.txt'), encoding='utf-8') as \
+        requirements_file:
+        requirements = requirements_file.read().splitlines()
+        return requirements
+
+setup(
+    name="sampleproject",
+    version="0.1.0",
+    author='Doyeon Kim',
+    author_email='archdo0909@gmail.com',
+    description="Easy and fast python SNS notifier.",
+    long_description=get_readme(),
+    long_description_content_type="test/markdown",
+    license="MIT",
+    install_requires=get_requirements(),
+    url='https://github.com/archdo0909/NotiPy',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ]
+)
