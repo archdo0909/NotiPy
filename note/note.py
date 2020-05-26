e69ad85ce77551507e66afb9031fd8e56499d944
Doyeon Kim <archdo0909@gmail.com>
2020-05-26 21:55:54 +0900

    Regist telegram python file path in note python file

--- note/note.py
+++ note/note.py
@@ -1,5 +1,6 @@
 from note.messenger.email.mail import Mail
 from note.messenger.line.line import Line
+from note.messenger.telegram.telegram import Telegram
 
 class NOTE:
 
@@ -13,3 +14,7 @@ class NOTE:
     def Line(self, API_ID, API_PASSWORD):
         print("Send Message through LINE")
         return Line(API_ID, API_PASSWORD)
+
+    def Telegram(self, API_ID, API_PASSWORD):
+        print("Send Message through Telegram")
+        return Telegram(API_ID, API_PASSWORD)
