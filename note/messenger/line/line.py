e791e57bda10101e3b6d2cd254af04fec1308cd5
Doyeon Kim <archdo0909@gmail.com>
2020-05-26 18:03:21 +0900

    Test Line modules

--- note/messenger/line/line.py
+++ note/messenger/line/line.py
@@ -7,17 +7,17 @@ from note.messenger.messenger import messenger
 class Line(messenger):
     def __init__(self, API_ID, API_PASSWORD):
         super().__init__(API_ID, API_PASSWORD)
-        
+        self._img = ""
+
     def print_all(self):
         print(self.API_PASSWORD)
         print(self.API_ID)
-        print(self._recevier)
         print(self._msg)
 
-    def send_message(self, img):
+    def send_message(self):
         TARGET_URL = "https://notify-api.line.me/api/notify"
         headers = {'Authorization': 'Bearer ' + self.API_PASSWORD}
-        payload = {'message': self._message}
+        payload = {'message': self._msg}
         files = {'imageFile': open(self._img, 'rb')} if self._img else None
         r = requests.post(TARGET_URL, headers=headers, params=payload, files=files)
         if files:
